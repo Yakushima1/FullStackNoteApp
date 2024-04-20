feb48e0678bb3ea55a1e32ff6c99f23977b08cf0
Yakushima1 <yurttoygar@hotmail.com>
2024-04-21 01:54:54 +0300

    UI part is done. Now user can edit their notes and delete them

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -40,10 +40,56 @@ const App = () => {
       setContent(note.content)
     }
 
+    const handleUpdateNote = (
+      event: React.FormEvent
+      )=>{
+        event.preventDefault();
+        if(!selectedNote){
+          return;
+        }
+        const updatedNote: Note = {
+          id: selectedNote.id,
+          title: title,
+          content: content
+        }
+
+        const updateNotesList = notes.map((note) => 
+          note.id === selectedNote.id 
+          ? updatedNote 
+          : note
+        )
+
+        setNotes(updateNotesList)
+        setTitle("")
+        setContent("")
+        setSelectedNote(null)
+      }
+
+      const handleCancle = () =>{
+        setTitle("")
+        setContent("")
+        setSelectedNote(null)
+      }
+
+      const deleteNote = (
+        event: React.MouseEvent,
+         noteId:number) => {
+          event.stopPropagation();
+
+          const updatedNotes = notes.filter(
+            (note) => note.id !== noteId
+          )
+
+          setNotes(updatedNotes);
+      }
+
   return(
     <div className="app-contanier">
       <form className="note-form" 
-      onSubmit={(event) => handleAddNote(event)}>
+      onSubmit={(event) => 
+      selectedNote 
+      ? handleUpdateNote(event) 
+      :handleAddNote(event)}>
         <input
           value={title}
           onChange={(event) => 
@@ -58,15 +104,23 @@ const App = () => {
              onChange={(event) => 
             setContent(event.target.value)
           }
-             placeholder="Context"
+             placeholder="Content"
              rows={10}
              required
              >
                  </textarea>
+            {selectedNote ? (
+              <div className="edit-buttons">
+                <button type = "submit">Save</button>
+                <button onClick={handleCancle}>Cancel</button>
+              </div>
+            ) : (
+              <button type = "submit">Add Note</button>
+            )}
 
-          <button 
-            type="submit">Add
-          </button>
+
+
+
         </form>
 
         <div className="notes-grid">
@@ -74,7 +128,8 @@ const App = () => {
                  <div className="note-item"
                  onClick={() => handleNoteClick(note)}>
                  <div className="notes-header">
-                   <button>X</button>
+                   <button onClick={(event)=>
+                  deleteNote(event, note.id)}>X</button>
                  </div>
                  <h2>{note.title}</h2>
                  <p>{note.content}</p>
